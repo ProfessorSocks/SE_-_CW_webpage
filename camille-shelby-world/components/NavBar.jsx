65f230b150af5574d44a2181282d33012762ead2
marionettebee <shelby.eckstrom@gmail.com>
2026-07-26 10:00:04 -0700

    commit

---
+++ camille-shelby-world/components/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar() {
+  return (
+    <nav>
+      <h1>My Website</h1>
+
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+    </nav>
+  );
+}
+
+export default NavBar;
--- camille-shelby-world/src/App.jsx
+++ camille-shelby-world/src/App.jsx
@@ -2,12 +2,58 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
+import NavBar from "../components/NavBar.jsx";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <>
+      <NavBar />
 
-  return <div style={{ fontSize: "64px", color: "forestgreen" }}>Welcome</div>;
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center", // Centers left/right
+          alignItems: "center",     // Centers up/down
+          minHeight: "100px",
+          border: "3px dashed green",
+          backgroundColor: "palegreen",
+          position: "relative",
+          zIndex: 9999,
+          fontSize: "64px",
+          color: "forestgreen",
+        }}
+      >
+        Hello!
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100px",
+          border: "3px dashed green",
+          backgroundColor: "palegreen",
+          position: "relative",
+          zIndex: 9999,
+        }}
+      >
+        <h1
+          style={{
+            color: "forestgreen",
+            opacity: 1,
+            visibility: "visible",
+            fontSize: "24px",
+            position: "static",
+          }}
+        >
+          Welcome to our website!
+        </h1>
+      </div>
+    </>
+  );
 }
+
 
 export default App;
